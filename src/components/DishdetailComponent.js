a990684e6912f019fed634b3b287bc948e8612ae
Vectoriia <viktoriamonastyrska@gmail.com>
2022-07-12 13:06:23 +0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Card, CardImg, 
-    //CardImgOverlay,
-     CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
     
 
     function RenderDish({dish}){
@@ -24,7 +23,7 @@ import { Card, CardImg,
             )
         }
     }
-    function RenderComents({comments}){
+    function RenderComments({comments}){
         if(comments==null){
             return(
                 <div></div>
@@ -68,10 +67,27 @@ import { Card, CardImg,
         }
 
         return(
-            <div className='row'>
-                <RenderDish dish={props.dish}/>
-                <RenderComents comments ={props.dish.comments}/>
-            </div>
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
         )
     }
 
